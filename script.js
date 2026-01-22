fe385fa359de1d8450c1d2d86b35c290f87a554f
hani7248390 <hani7248390@gmail.com>
2026-01-22 22:29:59 +0530

    Add login and messaging functionality

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+function login(){
+  if(password.value.length < 6){
+    strengthText.innerText = "Password too short";
+    return;
+  }
+  loginScreen.classList.add("hidden");
+  chatScreen.classList.remove("hidden");
+}
+
+function sendMsg(){
+  if(message.value==="") return;
+
+  let div = document.createElement("div");
+  div.className="msg user";
+  div.innerText = message.value;
+  chatArea.appendChild(div);
+
+  message.value="";
+
+  setTimeout(()=>{
+    let bot = document.createElement("div");
+    bot.className="msg bot";
+    bot.innerText="I’m listening 💕";
+    chatArea.appendChild(bot);
+  },800);
+}
+
+function changeBg(type){
+  chatArea.className="chat " + type;
+}
 
