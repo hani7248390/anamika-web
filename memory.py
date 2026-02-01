e7c6de1b28be981a528a4aaa59215edffbd58b82
hani7248390 <hani7248390@gmail.com>
2026-02-01 14:34:11 +0530

    Update memory.py

--- memory.py
+++ memory.py
@@ -1,11 +1,10 @@
-# Simple in-memory brain
 memory = []
 
-def save_memory(user_text, ai_text):
+def remember(user, ai):
     memory.append({
-        "user": user_text,
-        "ai": ai_text
+        "user": user,
+        "ai": ai
     })
 
-def get_memory():
-    return memory[-5:]
+def recall():
+    return memory[-10:]
